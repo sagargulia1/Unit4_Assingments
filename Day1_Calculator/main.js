a250fd34781b970c18a9ff05100be456e6eb0044
Sagar <96231430+sagargulia1@users.noreply.github.com>
2022-03-12 20:10:51 +0530

    first commit

---
+++ Day1_Calculator/main.js
@@ -0,0 +1,14 @@
+const add=require('./add')
+console.log(`Addition is = ${add(10,20)}`)
+
+const div=require('./divide')
+console.log(`Division is = ${div(10,20)}`)
+
+const mul=require('./multiply')
+console.log(`Multiplication is = ${mul(10,20)}`)
+
+const sub=require('./subtract')
+console.log(`Subtraction is = ${sub(10,20)}`)
+
+
+
